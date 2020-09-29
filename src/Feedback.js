598bad3e4f5c35f8d9fb87aae90355a98e407be4
Kuddleman <edl1155@gmail.com>
2020-09-29 14:38:34 -0700

    add useState to Feedback component

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Feedback = () => {
-  return(<div className="p-5">
-    <p>Hello from FEEDBACK</p>
-  </div>)
+
+    const [ values, setValues ] = useState({
+      name: '',
+      email: '',
+      message: '',
+      phone: '',
+      uploadedFiles: [],
+      buttonText: 'Submit',
+      uploadPhotoButtonText: 'Upload files'
+    })
+    
+  return (<div className="p-5">
+              <p>Hello from FEEDBACK</p>
+          </div>
+         )
 }
 
 
